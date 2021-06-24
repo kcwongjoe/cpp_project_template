fd8059525be0b59ad52de70e5235ee6edbfe891d
JOE <kcwong.joe@gmail.com>
2021-06-24 16:16:00 +0800

    Update Unittest desciption

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -1,15 +1,29 @@
 #include <gtest/gtest.h>
 
 /**
- * @brief Title: 
- * Description: 
- * Precondition: 
- * Assumption: 
- * Test Steps:
- * Expected Result:
- *  
+@brief 
+ * <pre>
+ * <b>TestID:</b> example_test01
+ * <b>Title:</b> Test Example
+ * </pre>
+ * 
+ * @details
+ * <pre>
+ * <b>Description:</b>
+ *   1. Do something
+ *   2. Do something
+ *   3. Do something
+ * <b>Precondition:</b>
+ * <b>Assumption:</b>
+ * <b>Test Steps:</b>
+ *   1. Test a == b
+ *   2. Test c > b
+ * <b>Expected Result:</b>
+ *   1. True
+ *   2. True
+ * </pre>
  */
 TEST(TestExample, Test) {
 
-    SUCCESS();
+    SUCCEED();
 }
